ce6c84c5c03700509827ea64784b51f661826abb
vicky <lch82327@gmail.com>
2016-07-14 16:02:16 +0800

    first commit

---
+++ app/views/lunches/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @lunch, :id, :created_at, :updated_at
---
+++ db/migrate/20160714075011_create_lunches.rb
@@ -0,0 +1,12 @@
+class CreateLunches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lunches do |t|
+      t.string :name, null: false
+      t.string :location
+      t.string :phone
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
